b4b589c8b480a30ba17bd63a542ad1ba75199d1b
AICHOUN-C <cherif.aichoun@gmail.com>
2020-09-30 16:55:01 +0200

    fix: missing countrie

--- random.js
+++ random.js
@@ -1,12 +1,10 @@
-let sources = ['allemagne', 'france', 'belgique', 'espagne', 'portugal', 'paysBas'];
+let sources = ['allemagne', 'france', 'angleterre', 'belgique', 'espagne', 'portugal', 'paysBas'];
 let randomLink = document.getElementById('random-country');
 
 randomLink.addEventListener('click', function (event) {
-    // event.preventDefault();
     randomCountry();
 } );
 
 function randomCountry () {
-    console.log('from randomCountry');
     location.href = 'countries/' + sources[Math.floor (Math.random()* (sources.length -1))] + '.html';
 }
