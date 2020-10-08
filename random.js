a6e0e7d4b49fca1e320c49d52887f4cc5c3dc14b
mickaelbell <mickael.belleuvre@gmail.com>
2020-10-08 10:07:34 +0200

    clean le code fait le carousel

--- random.js
+++ random.js
@@ -1,14 +1,14 @@
 let sources = ['allemagne', 'france', 'angleterre', 'belgique', 'espagne', 'portugal', 'paysBas'];
 let randomLink = document.getElementById('random-country');
-// let randomLinkBurger = document.getElementById('random-country-burger');
+let randomLinkBurger = document.getElementById('random-country-burger');
 
 randomLink.addEventListener('click', function (event) {
     randomCountry();
 } );
 
-// randomLinkBurger.addEventListener('click', function (event) {
-//     randomCountry();
-// } );
+randomLinkBurger.addEventListener('click', function (event) {
+    randomCountry();
+} );
 
 function randomCountry () {
     location.href = sources[Math.floor (Math.random()* (sources.length -1))] + '.html';
